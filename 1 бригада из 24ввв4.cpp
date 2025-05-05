c7852dda4da07aedd1f8a4148bd8838d4cad4e92
Artem <aartem_anisimov86@mail.ru>
2025-05-06 01:14:14 +0300

    добавил файл по дисциплине программирования

---
+++ 1 бригада из 24ввв4.cpp
@@ -0,0 +1,25 @@
+﻿// 1 бригада из 24ввв4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+double calculate_y(double x) {
+	// Функция рассчитывает значение y по формуле y = sqrt(2*x) + sin(|3*x|)/3.56
+	double root_part = sqrt(2 * x);
+	double absolute_value = fabs(3 * x);
+	double sine_part = sin(absolute_value);
+
+	return root_part + sine_part/ 3.56;
+}
+
+int main() {
+	for (int i = -5; i <= 5; ++i) {
+		printf("y(%d) = %.4lf\n", i,
+			calculate_y((double)i));
+	}
+
+	return 0;
+}
+
+
